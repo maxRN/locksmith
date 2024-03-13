e20ab4ad1379a09e5c2a275ec9c92944dbc53071
Max Große <max12.gro@gmail.com>
2024-03-13 21:33:52 +0100

    show dummy output

--- bin/main.ml
+++ bin/main.ml
@@ -20,16 +20,20 @@ let get_format format =
   f
 ;;
 
-let convert (input_format : formats option) file =
-  let output_format = OnePassword in
-  let input_format =
-    match input_format with
-    | Some x -> x
-    | _ -> failwith "error"
-  in
+let debug_info input output file =
+  print_endline
+    (Format.sprintf
+       "Converting %s from %s to %s\n"
+       file
+       (format_to_string input)
+       (format_to_string output))
+;;
+
+let convert input_format output_format file =
   let read = (get_format input_format).read in
   let to_string = (get_format output_format).to_string in
   let out = read file |> to_string in
+  debug_info input_format output_format file;
   print_endline out
 ;;
 
@@ -38,7 +42,13 @@ let convert (input_format : formats option) file =
 let input_format =
   let doc = "Input format" in
   let my_enum = Arg.enum (List.map (fun f -> format_to_string f, f) all_of_formats) in
-  Arg.(value & opt (some my_enum) None & info [ "i"; "f"; "from" ] ~doc)
+  Arg.(required & opt (some my_enum) None & info [ "i"; "f"; "from" ] ~doc)
+;;
+
+let output_format =
+  let doc = "Input format" in
+  let my_enum = Arg.enum (List.map (fun f -> format_to_string f, f) all_of_formats) in
+  Arg.(required & opt (some my_enum) None & info [ "t"; "to" ] ~doc)
 ;;
 
 let file = Arg.(value & pos 0 file "dune-project" & info [] ~docv:"FROM")
@@ -46,7 +56,7 @@ let file = Arg.(value & pos 0 file "dune-project" & info [] ~docv:"FROM")
 let cmd =
   let doc = "Transform between password manager export formats." in
   let info = Cmd.info "locksmith" ~version:"%%VERSION%%" ~doc in
-  Cmd.v info Term.(const convert $ input_format $ file)
+  Cmd.v info Term.(const convert $ input_format $ output_format $ file)
 ;;
 
 let main () = exit (Cmd.eval cmd)
--- lib/bitwarden.ml
+++ lib/bitwarden.ml
@@ -1,8 +1,4 @@
 let default : SharedEntry.entry = { username = "u"; password = "123" }
 let read file = if file == "123" then default else default
-
-let to_string (entry : SharedEntry.entry) =
-  if entry.username == "123" then "u are user" else "don't threaten me with a good time"
-;;
-
-let make : SharedEntry.format = { read = read; to_string = to_string }
+let to_string (_entry : SharedEntry.entry) = "bitwarden output"
+let make : SharedEntry.format = { read; to_string }
